d50c2f4a4034ab7c0f428b298f23d252940a4aaf
Carlos Ramirez <karlossander92@gmail.com>
2023-09-10 16:33:09 -0500

    Controlador Villa

---
+++ VillaMagica/VillaMagica_API/Controllers/VillaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VillaMagica_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+    }
+}

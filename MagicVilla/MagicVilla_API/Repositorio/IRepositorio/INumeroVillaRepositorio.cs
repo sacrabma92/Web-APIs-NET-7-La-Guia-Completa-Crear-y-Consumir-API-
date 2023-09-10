63017de3faca678bd0e817a92510c47dfa87fd7a
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2023-09-10 16:25:39 -0500

    Delete MagicVilla directory

--- MagicVilla/MagicVilla_API/Repositorio/IRepositorio/INumeroVillaRepositorio.cs
+++
@@ -1,9 +0,0 @@
-﻿using MagicVilla_API.Modelos;
-
-namespace MagicVilla_API.Repositorio.IRepositorio
-{
-    public interface INumeroVillaRepositorio : IRepositorio<NumeroVilla>
-    {
-        Task<NumeroVilla> Actualizar(NumeroVilla entidad);
-    }
-}
--- MagicVilla/MagicVilla_API/Repositorio/IRepositorio/IVillaRepositorio.cs
+++
@@ -1,9 +0,0 @@
-﻿using MagicVilla_API.Modelos;
-
-namespace MagicVilla_API.Repositorio.IRepositorio
-{
-    public interface IVillaRepositorio : IRepositorio<Villa>
-    {
-        Task<Villa> Actualizar(Villa entidad);
-    }
-}
